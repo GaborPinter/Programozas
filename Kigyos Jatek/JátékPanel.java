749f601153d5a4d1f3efb80947b31af8bb9f566e
GaborPinter <88519301+GaborPinter@users.noreply.github.com>
2022-02-24 21:21:07 +0100

    Update JátékPanel.java

--- Kigyos Jatek/JátékPanel.java
+++ Kigyos Jatek/JátékPanel.java
@@ -42,12 +42,6 @@ public class JátékPanel extends JPanel implements ActionListener{
 	public void draw(Graphics g) {
 		
 		if(futás) {
-			/*
-			for(int i=0;i<SCREEN_HEIGHT/UNIT_SIZE;i++) {
-				g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
-				g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
-			}
-			*/
 			g.setColor(Color.black);
 			g.fillOval(almaX, almaY, karakterMéret, karakterMéret);
 		
@@ -58,7 +52,6 @@ public class JátékPanel extends JPanel implements ActionListener{
 				}
 				else {
 					g.setColor(new Color(45,180,0));
-					//g.setColor(new Color(random.nextInt(255),random.nextInt(255),random.nextInt(255)));
 					g.fillRect(x[i], y[i], karakterMéret, karakterMéret);
 				}			
 			}
